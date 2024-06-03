fafe0b6a1dc14ba17e8d5c4f92e41062e4c6b0f5
linus <lagbleze@st.ug.edu.gh>
2024-06-03 22:49:34 +0200

    todo

--- explore_images.py
+++ explore_images.py
@@ -2,3 +2,11 @@
 #%%
 import pandas as pd
 
+
+#%%
+train_df = pd.read_csv("/home/lin/codebase/hse_count_prediction/zindi_hse_no_pred/Train.csv")
+
+# %%
+train_df.category_id.value_counts()
+# %%
+added visz for tif
