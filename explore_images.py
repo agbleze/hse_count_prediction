6bb4e5bc0fbc779a8fbdf8515a9632e5f1141b4e
linus <lagbleze@st.ug.edu.gh>
2024-06-04 23:46:52 +0200

    added get_tiff_img

--- explore_images.py
+++ explore_images.py
@@ -1,7 +1,7 @@
 
 #%%
 import pandas as pd
-
+import numpy as np
 
 #%%
 train_df = pd.read_csv("/home/lin/codebase/hse_count_prediction/zindi_hse_no_pred/Train.csv")
@@ -9,4 +9,29 @@ train_df = pd.read_csv("/home/lin/codebase/hse_count_prediction/zindi_hse_no_pre
 # %%
 train_df.category_id.value_counts()
 # %%
-added visz for tif
+#1 added visz for tif
+#2 
+
+def get_tiff_img(path, return_all_bands, bands=("B01", "B03", "B02"),
+                 normalize_bands=True
+                ):
+    all_band_names = ("B01","B02", "B03","B04","B05", "B06",
+                      "B07","B08","B8A","B09","B11","B12"
+                    )
+    if return_all_bands:
+        band_indexs = [all_band_names.index(band_name) for band_name in all_band_names]
+    
+    else:
+        band_indexs = [all_band_names.index(band_name) for band_name in bands]
+    #print(band_indexs)
+    with rasterio.open(path) as src:
+        img_bands = [src.read(band) for band in range(1,13)]
+    dstacked_bands = np.dstack([img_bands[band_index] for band_index in band_indexs])
+    #dstacked_bands = np.dstack([img_bands[3], img_bands[2], img_bands[1]])
+    if normalize_bands:
+        # Normalize bands to 0-255
+        dstacked_bands = ((dstacked_bands - dstacked_bands.min()) / 
+                          (dstacked_bands.max() - dstacked_bands.min()) * 255
+                          ).astype(np.uint8)
+
+    return dstacked_bands
